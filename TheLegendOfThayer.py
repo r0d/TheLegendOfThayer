a5106c76b9ca7be6514b473ce5796d03aae52382
r0d <nicolas.rodriguez@usma.edu>
2017-10-31 11:21:28 -0400

    Built outline script

---
+++ TheLegendOfThayer.py
@@ -0,0 +1,18 @@
+import PyGame
+import random
+
+def move_player(x, y, keypress): 
+	'''
+	returns new (x, y) tuple based on the current x/y and the key that was pressed to trigger the event
+	'''
+
+def do_damage(x, y, direction): 
+	'''
+	does damage to monster in a square based on current (x,y) and the direction that the player is facing 
+	''' 
+
+def update_screen('''It's going to need a lot of information'''): 
+	''' 
+	update the screen based on everything that happened in the last tick
+	'''
+
